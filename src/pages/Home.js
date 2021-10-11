aa1e3495c9b1916b6015cd7b0193cdeafebe4edf
salaami <salemsalleh@googlemail.com>
2021-10-11 18:23:40 +0200

    Housekeeping

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react' 
-import './Home.css'
 
 var Home = () =>{
     return (
         <div className='pages'>
             <div className='text-box'>
-                <h1 className='h1'> Hi,
+                <h1> Hi,
                     <h2 className='h2'>            
-                    my name is Salem.{<br/>}
-                    I'm a data engineer{<br/>}
-                    <h3 className="h3"> based in Hamburg.</h3>
+                        my name is Salem.{<br/>}
+                        I'm a data engineer{<br/>}
+                        <h3 className="h3"> 
+                            based in Hamburg.
+                        </h3>
                     </h2>    
                 </h1> 
             </div>
