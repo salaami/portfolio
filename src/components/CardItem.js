975f37daead9e1132221c8c229c5e95a343fdb8b
salaami <salemsalleh@googlemail.com>
2022-01-25 00:58:10 +0100

    Modify Projects Page.

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,18 +5,19 @@ import './CardItem.css'
 var CardItem = (props) => {
     return (
         <div className="card-container">
-            <div className="image-container">
-                <img src={props.imageUrl} alt='' />
-            </div>
-            <div className="card-content">
-                <div className="card-title">
-                    <h4>{props.title}</h4>
+            <a href={props.linkPath}>
+                <div className="image-container">
+                    <img src={props.imageUrl} alt='' />
                 </div>
-                <div className="card-body">
-                    <p>{props.body}</p>
+                <div className="card-content">
+                    <div className="card-title">
+                        <h4>{props.title}</h4>
+                    </div>
+                    <div className="card-body">
+                        <p>{props.body}</p>
+                    </div>
                 </div>
-            </div>
-        
+            </a>
         </div>
     )
 }
