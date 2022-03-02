30efc565c27829c541c856e280bbc63553fe592c
salaami <salemsalleh@googlemail.com>
2022-03-03 00:16:36 +0100

    Relocate Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
-
 
 var App = () => {
   return (
     <Router>
-      <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
-        <Route path='/projects' exact component={Projects} />
+        <Route path='/about' exact component={About} />
         <Route path='/contact' exact component={Contact} />
+        <Route path='/projects' exact component={Projects} />
       </Switch>
     </Router>
   );
