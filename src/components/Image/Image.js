2e159d5004e76af5bade3fadcf70895c365cbded
salaami <salemsalleh@googlemail.com>
2022-06-06 13:09:56 +0200

    Add alt to component.

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,7 +3,7 @@ import './Image.css'
 
 var Image = (props) => {
     return(
-            <img className="image" src={props.src}></img>
+            <img className="image" src={props.src} alt={props.description}></img>
     )
 }
 
