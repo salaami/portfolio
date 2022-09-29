4cda1e5d5e0e8fef347a73ba65ee87f3d16ed08b
salaami <salemsalleh@googlemail.com>
2022-09-30 00:06:49 +0200

    Layout of the contact page and some code cleasing.

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import LayoutProjects from '../../Layout/LayoutProjects'
+import './Projects.css'
+import { Navbar } from '../../Menu/Navbar'
+import Socialbar from '../../SocialBar/SocialBar'
+import Cards from '../../Gallery/Cards'
 
 var Projects = () => {
     return (
-        <LayoutProjects />
+        <div className="project-container">
+            <div className="sidebar-left">
+                <Navbar />
+            </div>
+            <div className="content">
+                <div className="project-content-box">
+                    <Cards /> 
+                </div>
+
+            </div>
+            <div className="sidebar-right">
+                <Socialbar />
+            </div>
+        </div>
     )
 }
 
