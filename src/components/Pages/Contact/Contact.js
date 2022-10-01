e97cc254da1f8be42c2dff46d41920b793f0c752
salaami <salemsalleh@googlemail.com>
2022-10-02 01:22:08 +0200

    Working over CSS and reducing redundancies.

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -5,28 +5,28 @@ import Navbar from "../../Menu/Navbar"
 
 export default function Contact() {
   return(
-    <div className="contact-container">
-        <div className="sidebar-left">
-            <Navbar />
-        </div>
-        <div className="content">
-            <div className="contact-content-box">
-              <div className="contact-text">
-              <h2 className="contact-header">Contact</h2>
+    <>
+      <header></header>
+      <body>
+          <div className="sidebar-left">
+              <Navbar />
+          </div>
+          <div className="contact-content">
+            <div className="contact-text">
+              <h1>Contact</h1>
                 <p>You have a question</p>
                 <p>and would like to reach out to me?</p>
                 <p>Then don't hesitate to send me a message and </p>
                 <p>I'll try to answer as soon as I can.</p>
                 <a className="contact-button" href="mailto:salemsalleh@gmail.com" rel="noreferrer" >say hello</a>
-              </div>
-    
             </div>
-
-        </div>
-        <div className="sidebar-right">
-            <Socialbar />
-        </div>
-    </div>
+          </div>
+          <div className="sidebar-right">
+              <Socialbar />
+          </div>
+      </body>
+      <footer></footer>
+    </>
   )
 }
 
--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -2,23 +2,29 @@ import React from 'react'
 import './Projects.css'
 import Navbar from '../../Menu/Navbar'
 import Socialbar from '../../SocialBar/SocialBar'
-import Cards from '../../Gallery/Cards'
+import Cards from './Cards'
 
 export default function Projects() {
     return (
-        <div className="project-container">
-            <div className="sidebar-left">
-                <Navbar />
-            </div>
-            <div className="content">
-                <div className="project-content-box">
-                    <Cards /> 
+        <>
+            <header></header>
+            <body>
+                <div className="sidebar-left">
+                    <Navbar />
                 </div>
-
-            </div>
-            <div className="sidebar-right">
-                <Socialbar />
-            </div>
-        </div>
+                    <div className="project-content">
+                        <h1>Projects</h1>
+                        <h2>
+                            <p>Check out the things I build.</p>
+                            <p>Here you'll find a collection of my projects.</p>
+                        </h2>
+                        <Cards /> 
+                    </div>
+                <div className="sidebar-right">
+                    <Socialbar />
+                </div>
+            </body>
+            <footer></footer>
+        </>
     )
 };
