11bb5896c571742e23b99b4035d1dd0b958cbafb
salaami <salemsalleh@googlemail.com>
2022-12-12 00:39:52 +0100

    Refines About text and fixes header position.

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -25,26 +25,30 @@ export default function About() {
         >
           <div className="about-text">
             <h1>ABOUT</h1>
-              <p>My journey started as a student of economics and business.
-              An introductory programming and data mining classes sparked my interest,
-              at a time when I was largley bored 
-              by the highly specialized topics of my buiness courses.
-              On the side I was earning some money as a working student.
-              I was supervised with tedious reporting tasks.
-              The limited capabilities of Microsoft Excel along with the mental pain it can cause
-              strengthened my interest in technologies that can ease it.
-              That was where I took the decision to redirect my career 
-              towards the data space after my business intelligence and data mining class.
-              Back then I wrote my thesis in that very field, 
-              hoping that I can land a related job afterwards.
-              As a graduate with limited hands-on experience 
-              I decided to take my first job as a system manager for media software.
-              While I was mainly busy with maintaing media tools and reporting tasks 
-              I got in touch with the BI department at my company.
-              Eventually the opportunity arouse to become a part of them.
-              Starting out I was using relational data base systems 
-              to mitigate the reporting efforts at my employer.
-
+              <p>My journey started as a student of economics and business. <br/>
+              Introductory programming and data science classes sparked my interest,<br/>
+              at a time when I was largely bored through out my business courses.<br/>
+              On the side I was earning some money as a working student.<br/>
+              My work largley consisted of tedious reporting tasks.<br/>
+              Before my Business Intelligence and Data Mining class <br/>
+              I was executing my duties just as my predecessors did.<br/>
+              But now I was empowered me to challenge the status quo<br/>
+              and escape the endless cycle of copy and paste in Microsoft Excel.<br/>
+              Drawn from these experiences and strengthened by the momentum the industry gained at that time<br/>
+              I took the decision to redirect my career towards the data space.<br/>
+              Hence I wrote my thesis at the department of business informatics.<br/>
+              My first full-time job was as a system manager at a media agency.<br/>
+              While I was mainly busy with maintaing media tools and reporting tasks <br/>
+              I quickly got in touch with the BI department.<br/>
+              Eventually the opportunity arouse to become a part of them.<br/>
+              Starting out I was using relational data base systems <br/>
+              to mitigate the reporting efforts of my employer.<br/>
+              Along with serious improvements of the infrastructure we were working on my tasks began to change.<br/>
+              Data provisioning became less relevant while data integration was gaining importance.<br/>
+              This was where learing curve got a lot steeper.<br/>
+              I was learning about workflows and technologies I never thought I would be using. <br/>
+              And now I am truly grateful to work in an environment <br/>
+              that encourages me to constantly learn new things and develop myself.<br/>
               </p>
 
           </div>
