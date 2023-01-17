58609d162fd7292a7af74253ec91facc236d6012
salaami <salemsalleh@googlemail.com>
2023-01-17 23:19:14 +0100

    Marquee refactoring.

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -6,6 +6,17 @@ import { PageVariants } from "../../components/Animations/PageVariants"
 import { motion } from "framer-motion"
 import { PageTransition } from "../../components/Animations/PageTransition"
 
+
+const hoverVariants = {
+  hover:{
+    textDecoration: "underline",
+    transition: {
+        delay: 0.2,
+        stiffness: 50,
+    }
+  }
+}
+
 export default function Contact() {
   return(
     <div>
@@ -17,9 +28,50 @@ export default function Contact() {
           transition={PageTransition}
           animate="animate"
         >
-          <h1 className="call-to-action">Say Hello</h1>
-          <a className="marquee" href="mailto:salemsalleh@gmail.com">
-            <p className="scrolling-text">SALEMSALLEH@GMAIL.COM</p>
+          <h2 
+            className="call-to-action"
+          >say hello
+          </h2>
+          <a 
+            className="marquee-container" 
+            href="mailto:salemsalleh@gmail.com"
+          >
+            <motion.span 
+              className="marquee"
+              whileHover="hover" 
+              variants={hoverVariants}
+            >SALEMSALLEH@GMAIL.COM
+            </motion.span>
+            <motion.span 
+              className="marquee"
+              whileHover="hover" 
+              variants={hoverVariants}
+            >SALEMSALLEH@GMAIL.COM
+            </motion.span>
+            <motion.span 
+              className="marquee"
+              whileHover="hover" 
+              variants={hoverVariants}
+            >SALEMSALLEH@GMAIL.COM
+            </motion.span>
+            <motion.span 
+              className="marquee"
+              whileHover="hover" 
+              variants={hoverVariants}
+            >SALEMSALLEH@GMAIL.COM
+            </motion.span>
+            <motion.span 
+              className="marquee"
+              whileHover="hover" 
+              variants={hoverVariants}
+            >SALEMSALLEH@GMAIL.COM
+            </motion.span>
+            <motion.span 
+              className="marquee"
+              whileHover="hover" 
+              variants={hoverVariants}
+            >SALEMSALLEH@GMAIL.COM
+            </motion.span>
           </a>
         </motion.div>
       </main>
