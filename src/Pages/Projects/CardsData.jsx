6faf42f7f95a0ccef8ecb31b226d5db3522ebb7b
salaami <salemsalleh@googlemail.com>
2023-03-13 00:21:38 +0100

    Define card layouts for different screens.

--- src/Pages/Projects/CardsData.jsx
+++ src/Pages/Projects/CardsData.jsx
@@ -3,21 +3,21 @@ import { DiReact } from 'react-icons/di'
 const CardsData = [
     {
         Id: 0,
-        Title: "PORTFOLIO",
+        Title: "portfolio",
         Text: "This website is a React application using jsx and css.",
         Link: "https://github.com/salaami/portfolio",
         Icon: <DiReact />
     },
     {
         Id: 1,
-        Title: "TEST",
+        Title: "test",
         Text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.", 
         Link: "https://github.com/salaami/",
         Icon: <DiReact />
     },
     {
         Id: 2,
-        Title: "TEST",
+        Title: "test",
         Text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
         Link: "https://github.com/salaami/",
         Icon: <DiReact />
