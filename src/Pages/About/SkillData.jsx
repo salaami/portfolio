ea25ce0250199e02ba516c00373b28cd5c244633
salaami <salemsalleh@googlemail.com>
2023-04-01 17:41:21 +0200

    Adds skills and technologie infos to about page.

---
+++ src/Pages/About/SkillData.jsx
@@ -0,0 +1,25 @@
+const SkillData = [
+  {
+    Id: 0,
+    Level: 90,
+    Title: "SQL",
+  },
+  {
+    Id: 1,
+    Level: 70,
+    Title: "Shell",
+  },
+  {
+    Id: 2,
+    Level: 60,
+    Title: "Python",
+  },
+  {
+    Id: 3,
+    Level: 40,
+    Title: "Javascript",
+  },
+]
+
+export { SkillData }
+
