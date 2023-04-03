51a25773c547a8ad2b5a8bb2256e0842dd2ec5c9
salaami <salemsalleh@googlemail.com>
2023-04-03 08:51:38 +0200

    Optimize content on about page.

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -14,34 +14,16 @@ export default function About() {
       exit="exit"
       variants={TransitionVariants}
     >
-      <h1>INTRO</h1>
       <article>
-        <p>
-          My journey started as a student of economics and business.
-          My interest was piqued by an introductory programming class,
-          at a time when I was largely bored by my business courses.
-          On the side I was earning some money as a working student.
-          My work largley consisted of tedious reporting responsibilities.
-          Before I took a Business Intelligence and Data Mining class,
-          I was executing my tasks just like my predecessors did.
-          But now I was empowered to challenge the status quo
-          and escape the endless cycle of copying and pasting in Microsoft Excel.
-          Drawn from these experiences and strengthened by the momentum the industry gained at that time
-          I decided to redirect my career towards the data space.
-          Hence I wrote my thesis at the department of business informatics.
-          My first full-time job was as a system manager at a media agency.
-          While I was mainly busy with maintaing media tools and reporting tasks
-          I quickly got in touch with the Business Intelligence department.
-          Eventually the opportunity arouse to become a part of them.
-          Starting out I was using relational data base systems
-          to mitigate the reporting efforts of my employer.
-          My tasks started to shift as our infrastructure underwent significant improvements.
-          Data provisioning became less relevant while data integration was gaining importance.
-          This was where learing curve got a lot steeper.
-          I was learning about workflows and technologies I never thought I would be using.
-          And now I am truly grateful to work in an environment
-          that encourages me to constantly learn new things and develop myself.
-        </p>
+        <h1>INTRO</h1>
+        I began as an economics and business student.
+        In my part-time job, I had to cope with tedious reporting tasks.
+        After taking a Business Intelligence and Data Mining class, I was able to automate and optimize these.
+        This experience motivated me to redirect my career towards the data space.
+        I wrote my thesis in business informatics and started my first full-time job as a system manager at a media agency.
+        I transitioned to the Business Intelligence department and began using relational databases to streamline reporting efforts.
+        As our infrastructure improved, I shifted my focus to data integration and data transformation tasks.
+        I found it very inspiring to work in an environment that encourages constant learning and development.
       </article>
       <Skills />
       <Tech />
