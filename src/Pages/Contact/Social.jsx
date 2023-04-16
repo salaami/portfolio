bc36f665e093b2355a98c3e3b1faa8286e74a34e
salaami <salemsalleh@googlemail.com>
2023-04-17 00:38:33 +0200

    Changes layout of contact page.

---
+++ src/Pages/Contact/Social.jsx
@@ -0,0 +1,49 @@
+import './Social.css'
+import React from 'react'
+import { motion } from 'framer-motion'
+import { SocialData } from './SocialData'
+
+const hoverState = {
+    hovered: {
+        scale: 0.95,
+        background: 'var(--accent-2)',
+    },
+    initial: {
+        scale: 1,
+        background: 'var(--darker)',
+    },
+    transition: {
+        duration: 0.3,
+        type: 'tween',
+        ease: 'easeInOut'
+    }
+};
+
+export default function Socialbar() {
+    return (
+        <div className="social">
+            {SocialData.map(({ Name, Icon, Id, Link }) => {
+                return (
+                    <motion.div
+                        className="item-wrapper"
+                        variants={hoverState}
+                        initial="initial"
+                        whileHover="hovered"
+                        whileTap="hovered"
+
+                    >
+                        <h2>{Name}</h2>
+                        <a
+                            key={Id}
+                            href={Link}
+                            className="social-icon"
+                            rel="noreferrer"
+                        >
+                            {Icon}
+                        </a>
+                    </motion.div>
+                )
+            })}
+        </div>
+    )
+}        
--- src/Pages/Contact/SocialData.jsx
+++ src/Pages/Contact/SocialData.jsx
@@ -1,32 +1,36 @@
-import { TiSocialLinkedin } from 'react-icons/ti' 
-import { TiSocialGithub } from 'react-icons/ti' 
-import { CgMail } from 'react-icons/cg' 
+import { TiSocialLinkedin } from 'react-icons/ti'
+import { TiSocialGithub } from 'react-icons/ti'
+import { CgMail } from 'react-icons/cg'
 import { FaXing } from 'react-icons/fa'
 
-const SocialBarData = [
+const SocialData = [
     {
         Id: 0,
+        Name: "email",
         Link: "mailto:salemsalleh@gmail.com",
         Cname: "social-icon",
         Icon: <CgMail />
     },
     {
         Id: 1,
+        Name: "github",
         Link: "https://www.github.com/salaami",
         Cname: "social-icon",
         Icon: <TiSocialGithub />
     },
     {
         Id: 2,
+        Name: "linkedin",
         Link: "https://www.linkedin.com/in/salem-salleh-1a4828a8",
         Cname: "social-icon",
         Icon: <TiSocialLinkedin />
     },
     {
         Id: 3,
+        Name: "xing",
         Link: "https://www.xing.com/profile/Salem_Salleh",
         Cname: "social-icon",
         Icon: <FaXing />
     },
 ]
-export { SocialBarData }
+export { SocialData }
