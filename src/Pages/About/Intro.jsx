1e301514d6de003537f4e0d368b5817ef798b0bc
salaami <salemsalleh@googlemail.com>
2023-11-11 14:42:04 +0100

    Separates the intro text to component and formates header and text.

---
+++ src/Pages/About/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Intro.css'
+
+
+export default function Intro() {
+  return (
+    <div>
+      <article className="intro-text">
+        <h1>Data-Driven Detour</h1>
+        <h2>Embracing constant change and development.</h2>
+        <p className="intro-paragraph">
+          I began as an economics and business student.
+          In my part-time job, I had to cope with tedious reporting tasks.
+          After taking a Business Intelligence and Data Mining class, I was able to automate and optimize these.
+          This experience motivated me to redirect my career towards the data space.
+          I wrote my thesis in business informatics and started my first full-time job as a system manager at a media agency.
+          I transitioned to the Business Intelligence department and began using relational databases to streamline reporting efforts.
+          As our infrastructure improved, I shifted my focus to data integration and data transformation tasks.
+          I found it very inspiring to work in an environment that encourages constant learning and development.
+        </p>
+      </article>
+    </div>
+  )
+}
