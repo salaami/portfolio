c405cf3b82256246e4617473d71b2f1d08c9c747
Salem Salleh <salem.salleh@getmercury.io>
2023-12-10 19:37:24 +0100

    Converts Project cards into list.

--- src/Pages/Projects/ListData.jsx
+++ src/Pages/Projects/ListData.jsx
@@ -1,38 +1,32 @@
-import { DiReact } from 'react-icons/di' 
-import { DiLinux } from 'react-icons/di'
-import { SiAirbyte } from 'react-icons/si'
-import { FaVuejs } from "react-icons/fa"
-
-
-const CardsData = [
+const ListData = [
     {
         Id: 0,
-        Title: "Portfolio",
+        Title: "Portfolio Website",
         Text: "This website is a React application using jsx and css.",
         Link: "https://github.com/salaami/portfolio",
-        Icon: <DiReact />
+        Tags: ["react.js", "jsx", "css"]
     },
     {
         Id: 1,
         Title: "Dotfiles",
         Text: "Version controlling my configuration files on Github.",
         Link: "https://github.com/salaami/dotfiles",
-        Icon: <DiLinux />
+        Tags: ["linux", "git", "bash"]
     },
     {
         Id: 2,
         Title: "Airbyte",
         Text: "Open-source data integration engine.",
         Link: "https://github.com/salaami/airbyte/tree/master",
-        Icon: <SiAirbyte />
+        Tags: ["python", "oop", "api", "http"] 
     },
     {
         Id: 3,
         Title: "Takraw Website",
         Text: "Small webapp for my sports club using Vue.js .",
         Link: "https://github.com/salaami/airbyte/tree/master",
-        Icon: <FaVuejs />
+        Tags: ["vue.js", "jsx", "css"]
     },
 ]
 
-export { CardsData }
+export { ListData }
