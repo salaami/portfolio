3a14539fffef3fd36bea9a11c6b7f51aebf85e18
Salem Salleh <salem.salleh@getmercury.io>
2024-07-15 23:49:48 +0200

    Removes cheap effects from Contact btns.

--- src/Animations/HoverVariants.jsx
+++ src/Animations/HoverVariants.jsx
@@ -1,7 +1,6 @@
 const HoverVariants = {
   hovered: {
     y: "-0.25rem",
-    backgroundImage: "linear-gradient(0deg, var(--grey-100), var(--highlight-2))",
     transition: {
         duration: 0.1,
         type: 'tween',
