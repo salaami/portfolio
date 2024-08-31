0f0874038d7b2a3f4e121ddf7e59cf65c94c4908
Salem Salleh <salem.salleh@getmercury.io>
2024-08-31 11:03:32 +0200

    Staggered anmimation for project list.

--- src/Pages/Projects/List.jsx
+++ src/Pages/Projects/List.jsx
@@ -1,43 +1,55 @@
-import { React } from 'react'
-import { ListData } from './ListData'
-import './List.css'
+import React from 'react';
+import { ListData } from './ListData';
+import { motion } from 'framer-motion';
+import './List.css';
 
+const staggerAnimation = {
+  initial: {
+    opacity: 0,
+    y: 55,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 export default function List() {
   return (
     <div className="list-container">
-
       {ListData.map((item, index) => {
         return (
-          <a
+          <motion.a
             className="list-item"
             href={item.Link}
             key={index}
+            variants={staggerAnimation}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={index}
           >
             <div className="list-pic">
               {item.Pic}
             </div>
-            <h5>
-              {item.Title}
-            </h5>
+            <h5>{item.Title}</h5>
             <div className="list-body">
-              <p className="list-text">
-                {item.Text}
-              </p>
+              <p className="list-text">{item.Text}</p>
               <p className="tags-container">
-                {item.Tags.map((Tags, Id) => (
-                  <span
-                    className="list-tags"
-                    key={Id}
-                  >
-                    {Tags}
+                {item.Tags.map((tag, id) => (
+                  <span className="list-tags" key={id}>
+                    {tag}
                   </span>
                 ))}
               </p>
             </div>
-          </a>
-        )
+          </motion.a>
+        );
       })}
     </div>
-  )
+  );
 }
+
